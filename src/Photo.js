40f7df1ff3369109ca7e0092ae7b0b25e79e6a15
Uros Drca <urosdrca22@gmail.comgit config --global user.name Uros>
2022-04-17 18:00:11 +0200

    Initial Commit

---
+++ src/Photo.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Photo = ({urls: { regular },
+    alt_description,
+    likes, 
+    user: {
+        name, 
+        portoflio_url, 
+        profile_image: { medium }, 
+    },
+}) => {
+    return <article className='photo'>
+        <img src={regular} alt={alt_description} />
+        <div className='photo-info'>
+            <div>
+                <h4>{name}</h4>
+                <p>{likes} likes</p>
+            </div>
+            <a href={portoflio_url}>
+                <img src={medium} alt={name} className='user-img' />
+            </a>
+        </div>
+    </article>
+};
+
+export default Photo;
